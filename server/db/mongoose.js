6f1542f9fe08f011987e8acd5a78d1ffb02b7c47
Honey Narendran <honey.narendran3@gmail.com>
2017-06-06 18:43:50 +0530

    App for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 
+const port = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+mongoose.connect(port);
 
 module.exports = {mongoose};
